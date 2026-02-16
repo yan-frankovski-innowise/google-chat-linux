0f59c545de7a35ab01990a095a92cfac512198a3
Yan Frankovski <yan.frankouski@innowise.com>
2026-02-16 15:20:13 +0100

    Fix for paths

--- src/paths.js
+++ src/paths.js
@@ -1,4 +1,5 @@
 const path = require("path");
+const fs = require("fs");
 const { app } = require("electron");
 
 let theme = 'default';
@@ -7,24 +8,34 @@ const evalIconPath = (theme, iconName) => {
     return eval(iconPathTemplate);
 }
 
+const getBasePath = () => {
+    if (process.resourcesPath) {
+        const testPath = path.resolve(process.resourcesPath, evalIconPath(theme, "normal-64.png"));
+        if (fs.existsSync(testPath)) {
+            return process.resourcesPath;
+        }
+    }
+    return path.join(__dirname, '..', 'assets');
+};
+
 const setIconTheme = (t) => {
     theme = t;
 }
 
 
 const normal = () => {
-    return path.resolve(process.resourcesPath, evalIconPath(theme, "normal-64.png"))
+    return path.resolve(getBasePath(), evalIconPath(theme, "normal-64.png"))
 }
 const badge = () => {
-    return path.resolve(process.resourcesPath, evalIconPath(theme, "badge-64.png"))
+    return path.resolve(getBasePath(), evalIconPath(theme, "badge-64.png"))
 }
 const offline = () => {
-    return path.resolve(process.resourcesPath, evalIconPath(theme, "offline-64.png"))
+    return path.resolve(getBasePath(), evalIconPath(theme, "offline-64.png"))
 }
 
 module.exports = {
     "configsPath": path.resolve(app.getPath("appData"), "google-hangouts-chat-linux.json"),
-    "OVERLAY_NEW_NOTIF": path.resolve(process.resourcesPath, "icon/overlay-new-xs.png"),
+    "OVERLAY_NEW_NOTIF": path.resolve(getBasePath(), "icon/overlay-new-xs.png"),
     normal: normal,
     badge: badge,
     offline: offline,
